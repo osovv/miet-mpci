11207fbf8cba7c8c2292694e1bc5cecd430cbbbd
Aleksey Chendemerov <chendemerov.aleksey@gmail.com>
2023-12-15 18:32:40 +0300

    LAB-1 Add example of usage in real life

--- 1-lab/app.js
+++ 1-lab/app.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const range = Array.from({ length: 151 }, (_, i) => i / 10); // Диапазон от 0 до 15
+  const range10 = range(1, 10, 0.1); // Диапазон от 0 до 15
+  const range15 = range(1, 15, 0.1); // Диапазон от 0 до 15
+  const range20 = range(-20, 20, 0.1); // Диапазон от 0 до 15
+  const range30 = range(-20, 30, 0.1); // Диапазон от 0 до 15
 
-  const triangleData = {
-    labels: range,
+  const trimfData = {
+    labels: range15,
     datasets: [
       {
         label: "Треугольная функция",
-        data: range.map((x) => triangularMembershipFunction(x, 2, 7, 12)), // Измененные параметры функции
+        data: range15.map((x) => trimf(x, 5, 7, 9)), // Измененные параметры функции
         fill: false,
         borderColor: "rgb(75, 192, 192)",
         tension: 0.1,
@@ -15,12 +18,12 @@ document.addEventListener("DOMContentLoaded", function () {
     ],
   };
 
-  const trapezoidData = {
-    labels: range,
+  const trapmfData = {
+    labels: range15,
     datasets: [
       {
         label: "Трапециевидная функция",
-        data: range.map((x) => trapezoidalMembershipFunction(x, 3, 5, 10, 12)), // Измененные параметры функции
+        data: range15.map((x) => trapmf(x, 5, 7, 9, 11)), // Измененные параметры функции
         fill: false,
         borderColor: "rgb(255, 99, 132)",
         tension: 0.1,
@@ -29,12 +32,12 @@ document.addEventListener("DOMContentLoaded", function () {
     ],
   };
 
-  const gaussianData = {
-    labels: range,
+  const gaussmfData = {
+    labels: range20,
     datasets: [
       {
         label: "Простая функция Гаусса",
-        data: range.map((x) => gaussianMembershipFunction(x, 7.5, 2)),
+        data: range20.map((x) => gaussmf(x, 4, 5)),
         fill: false,
         borderColor: "rgb(153, 102, 255)",
         tension: 0.1,
@@ -43,26 +46,42 @@ document.addEventListener("DOMContentLoaded", function () {
     ],
   };
 
-  const biGaussianData = {
-    labels: range,
+  const gauss2mfData = {
+    labels: range30,
     datasets: [
       {
-        label: "Двухсторонняя функция Гаусса",
-        data: range.map((x) => biGaussianMembershipFunction(x, 2, 4, 1, 8)),
+        label: "Двухсторонняя функция Гаусса [2 3 5 7]",
+        data: range30.map((x) => gauss2mf(x, 2, 3, 5, 7)),
+        borderColor: "red",
         fill: false,
-        borderColor: "rgb(255, 159, 64)",
+        tension: 0.1,
+        pointRadius: 0,
+      },
+      {
+        label: "Двухсторонняя функция Гаусса [2 4 5 4]",
+        data: range30.map((x) => gauss2mf(x, 2, 4, 5, 4)),
+        fill: false,
+        borderColor: "green",
+        tension: 0.1,
+        pointRadius: 0,
+      },
+      {
+        label: "Двухсторонняя функция Гаусса [5 7 2 3]",
+        data: range30.map((x) => gauss2mf(x, 5, 7, 2, 3)),
+        fill: false,
+        borderColor: "blue",
         tension: 0.1,
         pointRadius: 0,
       },
     ],
   };
 
-  const bellData = {
-    labels: range,
+  const gbellmfData = {
+    labels: range20,
     datasets: [
       {
         label: "Обобщенный колокол",
-        data: range.map((x) => generalizedBellMembershipFunction(x, 2, 3, 4)),
+        data: range20.map((x) => gbellmf(x, 2, 3, 4)),
         fill: false,
         borderColor: "rgb(54, 162, 235)",
         tension: 0.1,
@@ -71,12 +90,12 @@ document.addEventListener("DOMContentLoaded", function () {
     ],
   };
 
-  const sigmoidData = {
-    labels: range,
+  const sigmfData = {
+    labels: range15,
     datasets: [
       {
         label: "Основная односторонняя сигмоидная функция",
-        data: range.map((x) => sigmoidMembershipFunction(x, 3, 4)),
+        data: range15.map((x) => sigmf(x, 3, 4)),
         fill: false,
         borderColor: "rgb(255, 99, 132)",
         tension: 0.1,
@@ -85,12 +104,12 @@ document.addEventListener("DOMContentLoaded", function () {
     ],
   };
 
-  const biSigmoidData = {
-    labels: range,
+  const dsigmfData = {
+    labels: range15,
     datasets: [
       {
         label: "Двухсторонняя сигмоидная функция",
-        data: range.map((x) => biSigmoidMembershipFunction(x, 3, 4, 7, 8)),
+        data: range15.map((x) => dsigmf(x, 3, 4, 7, 8)),
         fill: false,
         borderColor: "rgb(54, 162, 235)",
         tension: 0.1,
@@ -99,12 +118,12 @@ document.addEventListener("DOMContentLoaded", function () {
     ],
   };
 
-  const asymBiSigmoidData = {
-    labels: range,
+  const psimgfData = {
+    labels: range15,
     datasets: [
       {
         label: "Несимметричная двухсторонняя сигмоидная функция",
-        data: range.map((x) => asymBiSigmoidMembershipFunction(x, 3, 4, 7, 8)),
+        data: range15.map((x) => psimgf(x, 3, 4, 7, 8)),
         fill: false,
         borderColor: "rgb(75, 192, 192)",
         tension: 0.1,
@@ -113,12 +132,12 @@ document.addEventListener("DOMContentLoaded", function () {
     ],
   };
 
-  const zFunctionData = {
-    labels: range,
+  const zmfData = {
+    labels: range15,
     datasets: [
       {
         label: "Полиномиальная Z-функция",
-        data: range.map((x) => zFunction(x, 3, 7)),
+        data: range15.map((x) => zmf(x, 2, 5)),
         fill: false,
         borderColor: "rgb(255, 206, 86)",
         tension: 0.1,
@@ -127,12 +146,12 @@ document.addEventListener("DOMContentLoaded", function () {
     ],
   };
 
-  const piFunctionData = {
-    labels: range,
+  const pimfData = {
+    labels: range15,
     datasets: [
       {
         label: "PI-функция",
-        data: range.map((x) => piFunction(x, 1, 4, 5, 10)),
+        data: range15.map((x) => pimf(x, 2, 5, 7, 10)),
         fill: false,
         borderColor: "rgb(153, 102, 255)",
         tension: 0.1,
@@ -141,12 +160,12 @@ document.addEventListener("DOMContentLoaded", function () {
     ],
   };
 
-  const sFunctionData = {
-    labels: range,
+  const smfData = {
+    labels: range15,
     datasets: [
       {
         label: "S-функция",
-        data: range.map((x) => sFunction(x, 5, 10)),
+        data: range15.map((x) => smf(x, 7, 10)),
         fill: false,
         borderColor: "rgb(255, 159, 64)",
         tension: 0.1,
@@ -155,87 +174,267 @@ document.addEventListener("DOMContentLoaded", function () {
     ],
   };
 
-  new Chart(document.getElementById("triangleChart"), {
+  const gaussmf27 = (x) => gaussmf(x, 2, 7);
+  const gaussmf35 = (x) => gaussmf(x, 3, 5);
+
+  const minimaxIntersectionData = {
+    labels: range10,
+    datasets: [
+      {
+        label: "gaussmf [2 7]",
+        data: range10.map(gaussmf27),
+        fill: false,
+        borderColor: "red",
+        borderDash: [5, 5],
+        tension: 0.1,
+        pointRadius: 0,
+      },
+      {
+        label: "gaussmf [3 5]",
+        data: range10.map(gaussmf35),
+        fill: false,
+        borderColor: "green",
+        borderDash: [5, 5],
+        tension: 0.1,
+        pointRadius: 0,
+      },
+      {
+        label: "Минимаксное пересечение",
+        data: minimaxIntersection(range10, gaussmf27, gaussmf35),
+        fill: false,
+        borderColor: "blue",
+        tension: 0.1,
+        pointRadius: 0,
+      },
+    ],
+  };
+
+  const minimaxUnionData = {
+    labels: range10,
+    datasets: [
+      {
+        label: "gaussmf [2 7]",
+        data: range10.map(gaussmf27),
+        fill: false,
+        borderColor: "red",
+        borderDash: [5, 5],
+        tension: 0.1,
+        pointRadius: 0,
+      },
+      {
+        label: "gaussmf [3 5]",
+        data: range10.map(gaussmf35),
+        fill: false,
+        borderColor: "green",
+        borderDash: [5, 5],
+        tension: 0.1,
+        pointRadius: 0,
+      },
+      {
+        label: "Минимаксное объединение",
+        data: minimaxUnion(range10, gaussmf27, gaussmf35),
+        fill: false,
+        borderColor: "blue",
+        tension: 0.1,
+        pointRadius: 0,
+      },
+    ],
+  };
+
+  const probabilisticIntersectionData = {
+    labels: range10,
+    datasets: [
+      {
+        label: "gaussmf [2 7]",
+        data: range10.map(gaussmf27),
+        fill: false,
+        borderColor: "red",
+        borderDash: [5, 5],
+        tension: 0.1,
+        pointRadius: 0,
+      },
+      {
+        label: "gaussmf [3 5]",
+        data: range10.map(gaussmf35),
+        fill: false,
+        borderColor: "green",
+        borderDash: [5, 5],
+        tension: 0.1,
+        pointRadius: 0,
+      },
+      {
+        label: "Вероятностное пересечение",
+        data: probabilisticIntersection(range10, gaussmf27, gaussmf35),
+        fill: false,
+        borderColor: "blue",
+        tension: 0.1,
+        pointRadius: 0,
+      },
+    ],
+  };
+
+  const probabilisticUnionData = {
+    labels: range10,
+    datasets: [
+      {
+        label: "gaussmf [2 7]",
+        data: range10.map(gaussmf27),
+        fill: false,
+        borderColor: "red",
+        borderDash: [5, 5],
+        tension: 0.1,
+        pointRadius: 0,
+      },
+      {
+        label: "gaussmf [3 5]",
+        data: range10.map(gaussmf35),
+        fill: false,
+        borderColor: "green",
+        borderDash: [5, 5],
+        tension: 0.1,
+        pointRadius: 0,
+      },
+      {
+        label: "Вероятностное объединение",
+        data: probabilisticUnion(range10, gaussmf27, gaussmf35),
+        fill: false,
+        borderColor: "blue",
+        tension: 0.1,
+        pointRadius: 0,
+      },
+    ],
+  };
+
+  const gaussmf26 = (x) => gaussmf(x, 2, 6);
+
+  const complementData = {
+    labels: range10,
+    datasets: [
+      {
+        label: "gaussmf [2 6]",
+        data: range10.map(gaussmf26),
+        fill: false,
+        borderColor: "blue",
+        borderDash: [5, 5],
+        tension: 0.1,
+        pointRadius: 0,
+      },
+      {
+        label: "Дополнение",
+        data: complement(range10, gaussmf26),
+        fill: false,
+        borderColor: "red",
+        tension: 0.1,
+        pointRadius: 0,
+      },
+    ],
+  };
+
+  new Chart(document.getElementById("trimfChart"), {
     type: "line",
-    data: triangleData,
+    data: trimfData,
   });
 
-  new Chart(document.getElementById("trapezoidChart"), {
+  new Chart(document.getElementById("trapmfChart"), {
     type: "line",
-    data: trapezoidData,
+    data: trapmfData,
   });
 
-  new Chart(document.getElementById("gaussianChart"), {
+  new Chart(document.getElementById("gaussmfChart"), {
     type: "line",
-    data: gaussianData,
+    data: gaussmfData,
   });
 
-  new Chart(document.getElementById("biGaussianChart"), {
+  new Chart(document.getElementById("gauss2mfChart"), {
     type: "line",
-    data: biGaussianData,
+    data: gauss2mfData,
   });
 
-  new Chart(document.getElementById("bellChart"), {
+  new Chart(document.getElementById("gbellmfChart"), {
     type: "line",
-    data: bellData,
+    data: gbellmfData,
   });
 
-  new Chart(document.getElementById("sigmoidChart"), {
+  new Chart(document.getElementById("sigmfChart"), {
     type: "line",
-    data: sigmoidData,
+    data: sigmfData,
   });
 
-  new Chart(document.getElementById("biSigmoidChart"), {
+  new Chart(document.getElementById("dsigmfChart"), {
     type: "line",
-    data: biSigmoidData,
+    data: dsigmfData,
   });
 
-  new Chart(document.getElementById("asymBiSigmoidChart"), {
+  new Chart(document.getElementById("psimgfChart"), {
     type: "line",
-    data: asymBiSigmoidData,
+    data: psimgfData,
   });
 
-  new Chart(document.getElementById("zFunctionChart"), {
+  new Chart(document.getElementById("zmfChart"), {
     type: "line",
-    data: zFunctionData,
+    data: zmfData,
   });
 
-  new Chart(document.getElementById("piFunctionChart"), {
+  new Chart(document.getElementById("pimfChart"), {
     type: "line",
-    data: piFunctionData,
+    data: pimfData,
   });
 
-  new Chart(document.getElementById("sFunctionChart"), {
+  new Chart(document.getElementById("smfChart"), {
     type: "line",
-    data: sFunctionData,
+    data: smfData,
+  });
+  new Chart(document.getElementById("minimaxIntersectionChart"), {
+    type: "line",
+    data: minimaxIntersectionData,
+  });
+  new Chart(document.getElementById("minimaxUnionChart"), {
+    type: "line",
+    data: minimaxUnionData,
+  });
+  new Chart(document.getElementById("probabilisticIntersectionChart"), {
+    type: "line",
+    data: probabilisticIntersectionData,
+  });
+  new Chart(document.getElementById("probabilisticUnionChart"), {
+    type: "line",
+    data: probabilisticUnionData,
+  });
+  new Chart(document.getElementById("complementChart"), {
+    type: "line",
+    data: complementData,
   });
 
-  function triangularMembershipFunction(x, a, b, c) {
+  function trimf(x, a, b, c) {
     return Math.max(Math.min((x - a) / (b - a), (c - x) / (c - b)), 0);
   }
 
-  function trapezoidalMembershipFunction(x, a, b, c, d) {
+  function trapmf(x, a, b, c, d) {
     return Math.max(
       Math.min(Math.min((x - a) / (b - a), 1), (d - x) / (d - c)),
       0
     );
   }
 
-  function gaussianMembershipFunction(x, c, sd) {
+  function gaussian(x, sd, c) {
     return Math.exp(-Math.pow(x - c, 2) / (2 * Math.pow(sd, 2)));
   }
 
-  function biGaussianMembershipFunction(x, sd1, c1, sd2, c2) {
+  function gaussmf(x, sd, c) {
+    return gaussian(x, sd, c);
+  }
+
+  function gauss2mf(x, sd1, c1, sd2, c2) {
     if (x >= c1 && x <= c2) {
       return 1;
     } else if (x < c1) {
-      return Math.exp(-Math.pow(x - c1, 2) / (2 * Math.pow(sd1, 2)));
+      return gaussian(x, sd1, c1);
     } else {
-      return Math.exp(-Math.pow(x - c2, 2) / (2 * Math.pow(sd2, 2)));
+      return gaussian(x, sd2, c2);
     }
   }
 
-  function generalizedBellMembershipFunction(x, a, b, c) {
+  function gbellmf(x, a, b, c) {
     return 1 / (1 + Math.pow(Math.abs((x - c) / a), 2 * b));
   }
 
@@ -243,19 +442,19 @@ document.addEventListener("DOMContentLoaded", function () {
     return 1 / (1 + Math.exp(-a * (x - c)));
   }
 
-  function sigmoidMembershipFunction(x, c, a) {
+  function sigmf(x, c, a) {
     return sigmoid(x, c, a);
   }
 
-  function biSigmoidMembershipFunction(x, c1, a1, c2, a2) {
+  function dsigmf(x, c1, a1, c2, a2) {
     return sigmoid(x, c1, a1) - sigmoid(x, c2, a2);
   }
 
-  function asymBiSigmoidMembershipFunction(x, c1, a1, c2, a2) {
+  function psimgf(x, c1, a1, c2, a2) {
     return sigmoid(x, c1, a1) * sigmoid(x, c2, a2);
   }
 
-  function zFunction(x, a, b) {
+  function zmf(x, a, b) {
     if (x <= a) {
       return 1;
     }
@@ -273,7 +472,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  function piFunction(x, a, b, c, d) {
+  function pimf(x, a, b, c, d) {
     if (x <= a) {
       return 0;
     }
@@ -303,7 +502,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  function sFunction(x, a, b) {
+  function smf(x, a, b) {
     if (x <= a) {
       return 0;
     }
@@ -320,4 +519,67 @@ document.addEventListener("DOMContentLoaded", function () {
       return 1 - 2 * Math.pow((x - b) / (b - a), 2);
     }
   }
+
+  function minimaxIntersection(xs, mf1, mf2) {
+    const cb = (x) => Math.min(mf1(x), mf2(x));
+    return xs.map(cb);
+  }
+
+  function minimaxUnion(xs, mf1, mf2) {
+    const cb = (x) => Math.max(mf1(x), mf2(x));
+    return xs.map(cb);
+  }
+
+  function probabilisticIntersection(xs, mf1, mf2) {
+    const cb = (x) => mf1(x) * mf2(x);
+    return xs.map(cb);
+  }
+
+  function probabilisticUnion(xs, mf1, mf2) {
+    const cb = (x) => mf1(x) + mf2(x) - mf1(x) * mf2(x);
+    return xs.map(cb);
+  }
+
+  function complement(xs, mf) {
+    const cb = (x) => 1 - mf(x);
+    return xs.map(cb);
+  }
 });
+
+function getPrecision(num) {
+  const numStr = num.toString();
+
+  // Check if the number is in exponential form
+  if (numStr.includes("e")) {
+    const parts = numStr.split("e");
+    const decimalPart = parts[0].split(".")[1] || "";
+    const exponent = parseInt(parts[1], 10);
+    if (exponent < 0) {
+      // Adjust precision for numbers in the form of 1.23e-4
+      return decimalPart.length - exponent;
+    }
+    // For positive exponents, precision is effectively 0
+    return 0;
+  }
+
+  // For non-exponential numbers
+  const decimalPosition = numStr.indexOf(".");
+  if (decimalPosition === -1) {
+    return 0; // No decimal point means no fractional part
+  }
+
+  return numStr.length - decimalPosition - 1;
+}
+
+function range(from, to, step) {
+  if (step <= 0) {
+    console.error("Step must be greater than 0.");
+    return [];
+  }
+
+  const result = [];
+  for (let i = from; i < to; i += step) {
+    result.push(i.toFixed(getPrecision(step)));
+  }
+  return result;
+}
